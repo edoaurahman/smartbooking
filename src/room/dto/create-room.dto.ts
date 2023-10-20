d0468d6e39d64fb7c17f1b6614ce7c7e7e7e0a2b
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-10-20 07:19:31 +0700

    update dto room

--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -3,6 +3,8 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateRoomDto {
   @ApiProperty({ default: 'Room 1' })
   room_name: string;
+  @ApiProperty({ default: 'available' })
+  status: string;
   @ApiProperty({ default: null })
   code: string;
 }
